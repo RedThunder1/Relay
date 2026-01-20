9e2ca58567141123a68d7e43f664e0e721406d6a
RedThunder1 <redthunderspartan@gmail.com>
2026-01-20 13:16:59 -0600

    did some style changes and started working on some functionality.

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,6 +7,24 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('Relay');
+  node_list_mode: boolean = true; //If true the list mode will display nodes. If not it will display channels
+
+  changeListMode(): void {
+    if (this.node_list_mode) {
+      //Displays channels
+
+    } else {
+      //Displays Nodes
+
+    }
+
+
+    this.node_list_mode = !this.node_list_mode
+  }
+
+
+
 }
