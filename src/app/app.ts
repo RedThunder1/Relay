79365b95db3ab7460da12fd35376b416357a6218
James <redthunderspartan@gmail.com>
2026-01-22 11:21:37 -0600

    simple messages can appear on screen now.

--- src/app/app.ts
+++ src/app/app.ts
@@ -13,18 +13,20 @@ export class App implements OnInit {
   container: HTMLElement | undefined;
   panel: HTMLElement | undefined;
   centerPage: HTMLElement | undefined;
-  messageBarChildren: HTMLElement[] = [];
+  messageInput: HTMLTextAreaElement | undefined;
+  messageTools: HTMLElement | undefined;
+  pageMessages: HTMLElement | undefined;
+
 
   ngOnInit(): void {
     this.container = document.getElementById('node_list_container')!!;
     this.panel = document.getElementById('channel_settings')!!;
     this.centerPage = document.getElementById('center_page')!!;
+    this.messageInput = document.getElementById('message_input')!! as HTMLTextAreaElement;
+    this.messageTools = document.getElementById('message_tools')!!;
+    this.pageMessages = document.getElementById('center_page_messages')!!;
 
     this.changeListMode('channels')
-
-    for (let child of document.getElementById('message_bar')!!.children) {
-      this.messageBarChildren.push(document.getElementById(child.id)!!);
-    }
   }
 
   changeListMode(mode: string): void {
@@ -54,7 +56,8 @@ export class App implements OnInit {
       this.panel!!.style.height = "";
       this.panel!!.style.width = "";
 
-      for (let child of this.messageBarChildren) { child.style.display = ""; }
+      this.messageInput!!.style.display = "";
+      this.messageTools!!.style.display = "";
 
       this.currentSettingsMode = '';
       return;
@@ -62,14 +65,45 @@ export class App implements OnInit {
       this.panel!!.style.height = this.centerPage!!.offsetHeight.toString() + "px";
       this.panel!!.style.width = "200px";
 
-      for (let child of this.messageBarChildren) { child.style.display = ''; }
+      this.messageInput!!.style.display = "";
+      this.messageTools!!.style.display = "";
     } else if (mode == 'settings') {
       this.panel!!.style.height = this.centerPage!!.offsetHeight.toString() + "px";
       this.panel!!.style.width = this.centerPage!!.offsetWidth.toString() + "px";
 
       //Hides message bar since channel isn't viewable with settings panel open
-      for (let child of this.messageBarChildren) { child.style.display = 'none'; }
+      this.messageInput!!.style.display = "none";
+      this.messageTools!!.style.display = "none";
     }
     this.currentSettingsMode = mode;
+  }
+
+  messageBoxHeight(): void {
+    console.log('messageBoxHeight');
+    this.messageInput!!.style.height = this.messageInput!!.scrollHeight + "px"
+  }
+
+  messageEnter(event: Event | undefined): void {
+    event?.preventDefault()
+    let keyevent = event!! as KeyboardEvent;
+
+    if (keyevent.key === 'Enter' || keyevent.keyCode === 13) {
+      if (this.messageInput!!.value.length == 0) { return }
+
+      this.sendMessage('RedThunder117', this.messageInput!!.value)
+    }
+    this.messageInput!!.value = '';
+  }
+
+  sendMessage(name:string, content: string): void {
+    let message = "<div class=\"user_message\"><div class=\"user_message_img\"></div><div class=\"user_message_name\">" + name + "</div><div class=\"user_message_content\">" + content + "</div></div>"
+    //server stuff here
+
+    this.addMessage(message);
+  }
+
+  addMessage(message: string): void {
+    console.log(message);
+    this.pageMessages!!.innerHTML += message;
   }
 }
